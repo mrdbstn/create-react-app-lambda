0a12bb83ffaea743fd877c1632a2046721c9b172
mrdbstn <mouradboustani77@gmail.com>
2021-11-21 16:45:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bulma/css/bulma.min.css";
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mt-5 columns is-centered">
+        <div className="column is-half">
+          <div className="title is-2 has-text-centered">Vind nummers van een bepaalde vacature in Carerix</div>
+          <input class="input" type="text" placeholder="Zoek een bedrijf waarvoor je matches wilt vinden."/>
+        </div>
+      </div>
     </div>
   );
 }
